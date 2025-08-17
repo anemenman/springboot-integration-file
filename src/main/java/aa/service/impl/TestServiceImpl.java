dd2bbb60d2dfae217dbd9473f00f5406ea33b16a
aa <joejaw101@gmail.com>
2025-08-17 18:18:07 +0300

    20250817 fileWriterGateway writeToFile

--- src/main/java/aa/service/impl/TestServiceImpl.java
+++ src/main/java/aa/service/impl/TestServiceImpl.java
@@ -1,6 +1,7 @@
 package aa.service.impl;
 
 import aa.dto.TestDTO;
+import aa.service.FileWriterGateway;
 import aa.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestServiceImpl implements TestService {
 
+    private final FileWriterGateway fileWriterGateway;
+
     @Override
     public void sendMessage(final TestDTO testDTO) {
+        log.info("TestServiceImpl sendMessage----------->: {}", testDTO);
+        fileWriterGateway.writeToFile("tst1.txt", testDTO.toString());
     }
 }
